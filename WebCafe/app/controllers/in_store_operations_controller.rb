20891bf2fd6f94c711bca0d7ea3a7047040c37fd
otakisan <takashi@aquamarine.local>
2015-02-07 13:48:52 +0900

    メニューページにサンプル選択肢を追加 サンプルのオーダーページを追加

--- WebCafe/app/controllers/in_store_operations_controller.rb
+++ WebCafe/app/controllers/in_store_operations_controller.rb
@@ -17,9 +17,32 @@ class InStoreOperationsController < ApplicationController
 
   def reply
     @customer_voice = params["customer_voice"]
-    logger.debug((@customer_voice ||= "")  + " was selected voice.")
-    if @customer_voice == '3' then
-      render 'menu'
+    if @customer_voice then
+      @customer_data = CustomerVoice.find(@customer_voice)
+      if @custmer_data then
+        logger.debug(@customer.voice)
+      end
+      logger.debug((@customer_voice ||= "")  + " was selected voice.")
+#      if @customer_voice == '3' then
+      if isnextformenu?(@customer_data.voice) then
+        render 'menu'
+      end
     end
+  end
+
+  def order
+    today_repeat = session["today_repeat"]
+    if !today_repeat then
+      today_repeat = 0
+    end
+    session["today_repeat"] = today_repeat + 1
+
+    logger.debug("today_repeat:" + today_repeat.to_s)
+
+    @product_id = params["order_product"]
+  end
+
+  def isnextformenu?(voice)
+    return voice.include?("Good")
   end
 end
--- WebCafe/lib/cafe/barista_voice_generator.rb
+++ WebCafe/lib/cafe/barista_voice_generator.rb
@@ -3,5 +3,21 @@ module Cafe
     def getvoice
       'barista voice !'
     end
+
+    def welcomevoice
+      isrev = self.isrevisit
+      logger.debug("isrev:" + isrev)
+      voice = "Thank you for coming."
+      if isrev then
+        voice = "Glad to see you again !"
+      end
+
+      return voice
+    end
+
+    def isrevisit
+      today_repeat = session['today_repeat']
+      return today_repeat > 0
+    end
   end
 end
